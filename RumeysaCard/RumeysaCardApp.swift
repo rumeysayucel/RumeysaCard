b232f3644305c348a5a85f814e3acdd26f3bd34d
Rumeysa Yücel <rumeysa.yucel.1996@gmail.com>
2021-07-08 09:08:00 +0300

    Initial Commit

---
+++ RumeysaCard/RumeysaCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  RumeysaCardApp.swift
+//  RumeysaCard
+//
+//  Created by Rumeysa Yücel on 8.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct RumeysaCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
